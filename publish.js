a419e7e43d0e85cb350714aa833cc097881b5831
Raynos <raynos2@gmail.com>
2012-12-31 12:21:31 -0800

    fixed publish

--- publish.js
+++ publish.js
@@ -3,25 +3,45 @@ var WriteStream = require("write-stream")
 module.exports = publish
 
 function publish(client, channel) {
-    var stream = WriteStream(write)
+    var push = Queue(sendItem)
+        , stream = WriteStream(push)
 
     return stream
 
-    function write(item) {
-        console.log("publishing", item)
+    function sendItem(item, callback) {
         client.publish({
             channel: channel
             , message: item
             , callback: handleError
         })
+
+        function handleError(info) {
+            if (info[0] === 0) {
+                var message = info[1] || "pubsub publish failed"
+                stream.emit("error", Error(message))
+            }
+
+            callback()
+        }
+    }
+}
+
+function Queue(action) {
+    var buffer = []
+
+    return push
+
+    function push(data) {
+        buffer.push(data)
+        if (buffer.length === 1) {
+            action(buffer[0], next)
+        }
     }
 
-    function handleError(info) {
-        if (info[0] !== 0) {
-            return
+    function next() {
+        buffer.shift()
+        if (buffer.length > 0) {
+            action(buffer[0], next)
         }
-
-        var message = info[1] || "pubsub publish failed"
-        stream.emit("error", Error(message))
     }
 }
